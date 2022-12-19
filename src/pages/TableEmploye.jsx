31abcc9ed51263f9f00db3e2473fd6a8220cc83e
oubenali mohamed <oubenali15@gmail.com>
2022-12-19 14:46:51 +0100

    modification de la function filter dans la librairie table

--- src/pages/TableEmploye.jsx
+++ src/pages/TableEmploye.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import TableEmployee from '../../node_modules/ma-librairie-table/dist/components/TableEmployee'
+import TableEmployee from '../../node_modules/table-library-oc/dist/components/TableEmployee'
 import '../utils/styles.css'
 
 // récupération du tableau dans le localStorage
@@ -29,7 +29,6 @@ function TableEmploye() {
   return (
     <div className="table">
       <TableEmployee columns={columnsTable} rows={rows} />
-
       <NavLink className="link_home_employe" to="/">
         Home
       </NavLink>
